574ee7908943462ec6b025b26ede03608d3c1e87
Danan Wijaya <dananwijaya1996@gmail.com>
2020-10-28 18:40:21 +0700

    analyz code with rubocop

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,8 +1,12 @@
 class TermsController < ApplicationController
-
   def index
     @terms = Term.all
     render json: @terms
   end
 
+  private
+
+  def term_params
+    params.require(:term).permit(:d, :b, :dd)
+  end
 end
